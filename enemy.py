630c5635012fc367c20de1961e61eb6c0c7bf0aa
Dustin Krol <dustinkrol@gmail.com>
2025-09-28 13:32:33 -0400

    Added "fast" enemy type

--- enemy.py
+++ enemy.py
@@ -1,16 +1,34 @@
-import pygame
+import pygame, random
 from constants import *
 from math import sqrt
 
 class Enemy:
-    def __init__(self, x, y):
+    def __init__(self, x, y, player_level):
         self.x = x
         self.y = y
         self.color = RED
         self.radius = 15
         self.score_value = 200
+        self.dmg = 10
         self.health = 50
         self.speed = 2.5
+
+        if player_level == 2:
+            if random.randint(0, 2) == 0:
+                self.color = ORANGE
+                self.radius = 10
+                self.score_value = 250
+                self.dmg = 5
+                self.health = 50
+                self.speed = 5
+        elif player_level >= 3:
+            if random.randint(0, 1) == 0:
+                self.color = ORANGE
+                self.radius = 10
+                self.score_value = 250
+                self.dmg = 5
+                self.health = 50
+                self.speed = 5
     
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
